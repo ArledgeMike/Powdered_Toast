9d910f885bdc8a305455384463f66824b72ac0c2
Mike Arledge <marledge@darth-vader.clc1.net>
2015-03-26 16:45:54 -0500

    first commit of PT

---
+++ powdered_test.js
@@ -0,0 +1,59 @@
+﻿function Powdered_Toast(size, type, location, content){
+  this.size = size;
+  this.location = location;
+  this.content = content;
+  this.type = type;
+  this.toast_el = $('<div id="toast"/>');
+  this.$toast;
+  this.anim;
+  this.init();
+};
+
+Powdered_Toast.prototype = {
+  constructor: Powdered_Toast,
+  init:function(){
+    this.toast_el.addClass(this.size).
+      addClass(this.type).
+      addClass(this.location).
+      html(this.content).
+      appendTo('body');
+    
+    this.$toast = $('#toast');
+    this.$toast.on("click", this.destroy_toast.bind(this));
+    this.anim = document.getElementById("toast");
+    this.prefixed_event( this.anim, "AnimationEnd", "add", this.animate_toast_out.bind(this));
+
+    this.animate_toast_in();
+  },
+  
+  animate_toast_in:function(){
+    this.$toast.addClass("active_toast");
+  },
+  
+  animate_toast_out:function(){
+    var t = this;
+
+    window.setTimeout(function(){  
+     t.$toast.addClass("inactive_toast"); 
+     t.prefixed_event( t.anim, "AnimationEnd", "remove", t.animate_toast_in.bind(t));
+     t.prefixed_event( t.anim, "AnimationEnd", "add", t.destroy_toast.bind(t));
+
+    }, 2000);
+  },
+  
+  destroy_toast:function(){
+    this.$toast.remove();
+  },
+  
+  prefixed_event:function(element, type, action, callback) {
+    var pfx = ["webkit", "moz", "MS", "o", ""];
+    for (var p = 0; p < pfx.length; p++) {
+		  if (!pfx[p]) type = type.toLowerCase();
+		  if(action == "add"){
+        element.addEventListener(pfx[p]+type, callback, false);
+      }else{
+        element.addEventListener(pfx[p]+type, callback, false);
+      }
+	  }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,51 @@
+Powdered-Toast
+===========
+<img src ="https://raw.githubusercontent.com/ArledgeMike/Meat-Grinder/master/images/meat-grinder.png" />
+
+
+<h2>JS Toast Notifications</h2> <p>Version 1.0 - marble rye</p>
+<p>A js based toast notification system.</p>
+<p>Need to let someone know that their task is complete? Want to notify someone without shutting down the whole experience? Modal windows just not what you are looking for?</p>
+
+<h3>Dependencies</h3>
+<ul>
+<li>JQuery 1.9.1 or greater</li>
+<li>powdered_toast.js</li>
+<li>powdered_toast.css</li>
+</ul>
+
+<h3>Usasge</h3>
+<p>var toast = new Powdered_Toast( "size", "type", "location", "content");</p>
+<p>Type:(string)</p> 
+<ul>
+  <li>"success"</li>
+  <li>"error"</li>
+  <li>"info"</li>
+</ul>
+<p>Size:(string)</p>
+<ul>
+  <li>"full"</li>
+  <li>"large"</li>
+  <li>"medium"</li>
+  <li>"small"</li>
+</ul>
+<p>Location:(string)</p>
+<ul>
+  <li>"top left"</li>
+  <li>"top center"</li>
+  <li>"top right"</li>
+  <li>"bottom left"</li>
+  <li>"bottom center"</li>
+  <li>"bottom right"</li>
+</ul>
+<p>Content:(string)</p>
+<ul>
+  <li>Any html formatted string can go in here.</li>
+</ul>
+
+<h3>Customization</h3>
+<p>Animation and style customization can be done throught the included css file.</p>
+
+
+<h3>For the future</h3>
+<p>Dynamic ID's so that more than one toast note can be added if needed.</p> 
